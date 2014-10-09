50bd5037f241aac9f43e04ab6ea4b8f0866c015b
Hart-Sturgeon-Reed <hs14jm@student.ocadu.ca>
2014-10-08 22:39:58 -0400

    Update spaceCat.ino

--- spaceCat.ino
+++ spaceCat.ino
@@ -1,19 +1,16 @@
-const int btn = 3;
+const int btn = 3; //Define the external push button pin
 
 void setup() {
-  pinMode(btn,INPUT);
-  pinMode(13,OUTPUT); 
+  pinMode(btn,INPUT);//Initialize it as an input
   Serial.begin(9600);
   //Serial.println("hello space world");
-
 }
 
 void loop() {
-  if(digitalRead(btn)==HIGH) {
+  if(digitalRead(btn)==HIGH) { //If the button is pressed, send a signal to the serial port
     Serial.println("H");
   }else{
     Serial.println("L");
   }
-   
-  delay(100);
+  delay(100);//Wait a bit
 }
