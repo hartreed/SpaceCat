77aa7ad4914753cea2f967c1bf9201b6fd223219
Hart-Sturgeon-Reed <hs14jm@student.ocadu.ca>
2014-10-19 15:44:03 -0400

    Removed test functions

--- serial.php
+++ serial.php
@@ -5,7 +5,6 @@
     $comPort = "/dev/cu.usbmodem1411"; //The correct usb port
     $handle = fopen($comPort, "r"); //Open it as if it were a file, with read only access
     sleep(0.4); //Give the arduino a second to get ready to send
-    //fwrite($handle, "Hello space world");
     $serial = fread($handle, 1); //Pull a single byte of data from the port
     fclose($handle); //Close the connection
     if(!$serial){echo "L";}else{echo $serial;} //If you got a high signal 'H', echo that, otherwise echo 'L'
